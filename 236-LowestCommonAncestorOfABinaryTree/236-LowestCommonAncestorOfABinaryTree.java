0ee4fe0274b0c9458d2cb902e269490959967bf0
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 10:33:19 +0530

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ 236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,12 @@
+// Last updated: 6/4/2026, 10:33:18 AM
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}

e3af5e5a1fffdfe4ceca601a7697a4b5b9e7aa8e
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 10:33:06 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 6/4/2026, 10:33:05 AM
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int left=0;
+        for(int right=0;right<arr.length;right++){
+            if(arr[right]!=0){
+                int temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+                left++;
+            }
+        }
+    }
+}

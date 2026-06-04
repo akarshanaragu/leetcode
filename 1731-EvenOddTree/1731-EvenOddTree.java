0333fd78be221eb6bc54ad0a75bcfef93461e070
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 10:30:38 +0530

    Create 1731-EvenOddTree

---
+++ 1731-EvenOddTree/1731-EvenOddTree.java
@@ -0,0 +1,29 @@
+// Last updated: 6/4/2026, 10:30:37 AM
+class Solution {
+  public boolean isEvenOddTree(TreeNode root) {
+    var queue = new ArrayDeque<TreeNode>();
+    queue.offer(root);
+
+    var even = true;
+
+    while (!queue.isEmpty()) {
+      var prev = even ? -1 : Integer.MAX_VALUE;
+
+      for (var i = queue.size(); i > 0; i--) {
+        var node = queue.poll();
+
+        if (even) {
+          if (node.val % 2 == 0 || node.val <= prev) return false;
+        } else {
+          if (node.val % 2 == 1 || node.val >= prev) return false;
+        }
+        prev = node.val;
+
+        if (node.left != null) queue.offer(node.left);
+        if (node.right != null) queue.offer(node.right);
+      }
+      even = !even;
+    }
+    return true;
+  }
+}

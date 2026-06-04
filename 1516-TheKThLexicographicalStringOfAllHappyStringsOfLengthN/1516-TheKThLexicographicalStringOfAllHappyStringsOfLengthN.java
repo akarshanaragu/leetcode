96b8948eb210a1d9520ddd1e15ac0383cecdf8c5
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 10:30:55 +0530

    Create 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN

---
+++ 1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN/1516-TheKThLexicographicalStringOfAllHappyStringsOfLengthN.java
@@ -0,0 +1,37 @@
+// Last updated: 6/4/2026, 10:30:54 AM
+class Solution {
+    private Set<String> st;
+
+    public String getHappyString(int n, int k) {
+        st = new TreeSet<>();
+
+        find("", n);
+
+        if (st.size() < k) return "";
+
+        int idx = 0;
+        for (String str : st) {
+            if (idx == k - 1) {
+                return str;
+            }
+            idx++;
+        }
+
+        return "";
+    }
+
+    private void find(String str, int n) {
+
+        if (str.length() == n) {
+            st.add(str);
+            return;
+        }
+
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+
+            if (str.length() == 0 || ch != str.charAt(str.length() - 1)) {
+                find(str + ch, n);
+            }
+        }
+    }
+}

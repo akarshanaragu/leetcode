fea3c07aaf749de82eec37133bf659b5e8b69847
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 23:16:02 +0530

    File updated at 4/6/2026, 11:16:01 pm

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,19 +1,25 @@
-// Last updated: 6/4/2026, 10:33:43 AM
-class Solution {
-    public void rotate(int[] nums, int k) {
-        k %= nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
-
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
-    }
-}
+// Last updated: 4/6/2026, 11:16:01 pm
+1class Solution {
+2
+3    public void rotate(int[] a, int k) {
+4
+5        k %= a.length;
+6
+7        rev(a, 0, a.length - 1);
+8        rev(a, 0, k - 1);
+9        rev(a, k, a.length - 1);
+10    }
+11
+12    public void rev(int[] a, int l, int r) {
+13
+14        while (l < r) {
+15
+16            int t = a[l];
+17            a[l] = a[r];
+18            a[r] = t;
+19
+20            l++;
+21            r--;
+22        }
+23    }
+24}

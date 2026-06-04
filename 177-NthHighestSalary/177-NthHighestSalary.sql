d8ad0f9832e67d7ce3e65b65895c9be1cf6341b3
Akarshana R <akarshanaraguu@gmail.com>
2026-06-04 10:33:49 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,16 @@
+-- Last updated: 6/4/2026, 10:33:48 AM
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) 
+RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  IF N <= 0 THEN
+    RETURN QUERY SELECT NULL::INT;
+  ELSE
+    RETURN QUERY
+    SELECT DISTINCT e.salary
+    FROM Employee e
+    ORDER BY e.salary DESC
+    OFFSET N-1
+    LIMIT 1;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;

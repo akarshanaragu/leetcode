7495f253d69acff8146d2993d81736ca1034caef
Akarshana R <akarshanaraguu@gmail.com>
2026-06-12 23:05:28 +0530

    File updated at 12/6/2026, 11:05:26 pm

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,26 +1,17 @@
-// Last updated: 12/6/2026, 10:39:03 pm
+// Last updated: 12/6/2026, 11:05:26 pm
 1class Solution {
-2    public int search(int[] nums, int target) {
-3        int l = 0, r = nums.length - 1;
-4        while (l <= r) {
-5            int m = l + (r - l) / 2;
-6            if (nums[m] == target) return m;
-7
-8            if (nums[m] >= nums[l]) {
-9                if (target >= nums[l] && target < nums[m]) {
-10                    r = m - 1;
-11                } else {
-12                    l = m + 1;
-13                }
-14            } 
-15            else {
-16                if (target > nums[m] && target <= nums[r]) {
-17                    l = m + 1;
-18                } else {
-19                    r = m - 1;
-20                }
-21            }
-22        }
-23        return -1;
-24    }
-25}
+2    public boolean isPalindrome(String s) {
+3        s = s.toLowerCase();
+4        s = s.replaceAll("[^a-z0-9]", "");
+5
+6        int i = 0, j = s.length() - 1;
+7        while (i < j) {
+8            if (s.charAt(i) != s.charAt(j)) {
+9                return false;
+10            }
+11            i++;
+12            j--;
+13        }
+14        return true;
+15    }
+16}

eb2c43cbe7a9da5dad4b4518bfb5bc994ff2ee29
Akarshana R <akarshanaraguu@gmail.com>
2026-06-18 23:13:44 +0530

    File updated at 18/6/2026, 11:13:43 pm

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,36 +1,38 @@
-// Last updated: 6/6/2026, 10:42:27 pm
+// Last updated: 18/6/2026, 11:13:43 pm
 1class Solution {
-2    public List<Integer> spiralOrder(int[][] a) {
-3        int l = 0, t = 0;
-4        int r = a[0].length - 1;
-5        int b = a.length - 1;
-6        ArrayList<Integer> res = new ArrayList<>();
-7        while (t <= b && l <= r) {
+2    public List<Integer> spiralOrder(int[][] matrix) {
+3        int left = 0, top = 0;
+4        int right = matrix[0].length - 1;
+5        int bottom = matrix.length - 1;
+6
+7        ArrayList<Integer> res = new ArrayList<>();
 8
-9            for (int i = l; i <= r; i++) {
-10                res.add(a[t][i]);
-11            }
-12            t++;
-13
-14            for (int i = t; i <= b; i++) {
-15                res.add(a[i][r]);
-16            }
-17            r--;
-18
-19            if (t <= b) {
-20                for (int i = r; i >= l; i--) {
-21                    res.add(a[b][i]);
-22                }
-23                b--;
-24            }
-25
-26            if (l <= r) {
-27                for (int i = b; i >= t; i--) {
-28                    res.add(a[i][l]);
-29                }
-30                l++;
-31            }
-32        }
-33        return res;
-34    }
-35}
+9        while (top <= bottom && left <= right) {
+10
+11            for (int i = left; i <= right; i++) {
+12                res.add(matrix[top][i]);
+13            }
+14            top++;
+15
+16            for (int i = top; i <= bottom; i++) {
+17                res.add(matrix[i][right]);
+18            }
+19            right--;
+20
+21            if (top <= bottom) {
+22                for (int i = right; i >= left; i--) {
+23                    res.add(matrix[bottom][i]);
+24                }
+25                bottom--;
+26            }
+27
+28            if (left <= right) {
+29                for (int i = bottom; i >= top; i--) {
+30                    res.add(matrix[i][left]);
+31                }
+32                left++;
+33            }
+34        }
+35        return res;
+36    }
+37}

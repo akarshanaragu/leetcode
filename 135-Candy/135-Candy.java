975362a966bf6f8075551390e7544316d0839f40
Akarshana R <akarshanaraguu@gmail.com>
2026-07-13 14:49:04 +0530

    File updated at 13/7/2026, 2:49:03 pm

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,26 +1,30 @@
-// Last updated: 6/4/2026, 10:34:17 AM
-class Solution {
-    public int candy(int[] r) {
-        int n = r.length;
-        int count = 0;
-        int[] c = new int[n];
-        for (int i = 0; i < n; i++) {
-            c[i] = 1;
-        }
-        for (int i = 1; i < n; i++) {
-            if (r[i] > r[i - 1]) {
-                c[i] = c[i - 1] + 1;
-            }
-        }
-        for (int i = n - 1; i > 0; i--) {
-            if (r[i] < r[i - 1]) {
-                c[i - 1] = Math.max(c[i] + 1, c[i - 1]);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            count += c[i];
-        }
-        return count;
-
-    }
-}
+// Last updated: 13/7/2026, 2:49:03 pm
+1class Solution {
+2    public int candy(int[] rt) {
+3        int n = rt.length;
+4        int count = 0;
+5        int c[] = new int[n];
+6        for (int i = 0; i < n; i++) {
+7            c[i] = 1;
+8        }
+9
+10        for (int i = 1; i < n; i++) {
+11            if (rt[i] > rt[i - 1]) {
+12                c[i] = c[i - 1] + 1;
+13            }
+14        }
+15
+16        for (int i = n - 1; i > 0; i--) {
+17            if (rt[i] < rt[i - 1]) {
+18                c[i - 1] = Math.max(c[i - 1], c[i] + 1);
+19            }
+20        }
+21
+22        for (int i = 0; i < n; i++) {
+23            count += c[i];
+24        }
+25
+26        return count;
+27
+28    }
+29}

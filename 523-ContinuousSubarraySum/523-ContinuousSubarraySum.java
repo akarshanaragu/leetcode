7ded7cda1bd3b320e266ad6fc5de5055b27c1a44
Akarshana R <akarshanaraguu@gmail.com>
2026-07-17 14:23:59 +0530

    File updated at 17/7/2026, 2:23:59 pm

--- 523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
@@ -1,23 +1,25 @@
-// Last updated: 6/4/2026, 10:32:19 AM
-public class Solution {
-    public boolean checkSubarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        int sum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            int rem = k == 0 ? sum : sum % k;
-
-            if (map.containsKey(rem)) {
-                if (i - map.get(rem) > 1) {
-                    return true;
-                }
-            } else {
-                map.put(rem, i);
-            }
-        }
-
-        return false;
-    }
-}
+// Last updated: 17/7/2026, 2:23:59 pm
+1class Solution {
+2    public boolean checkSubarraySum(int[] arr, int k) {
+3        int n = arr.length, sum = 0;
+4        HashMap<Integer, Integer> map = new HashMap<>();
+5        map.put(0, -1);
+6        for (int i = 0; i < n; i++) {
+7            sum += arr[i];
+8            int r = sum % k;
+9
+10            if (map.containsKey(r)) {
+11                if (i - map.get(r) >= 2)
+12                    return true;
+13
+14            } else {
+15
+16                map.put(r, i);
+17            }
+18
+19        }
+20        return false;
+21
+22    }
+23}
+24

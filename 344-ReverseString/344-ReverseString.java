e09b4e84569a7efe0097704c7788a1d9218ef86f
Akarshana R <akarshanaraguu@gmail.com>
2026-07-20 14:01:51 +0530

    File updated at 20/7/2026, 2:01:49 pm

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,12 +1,15 @@
-// Last updated: 6/4/2026, 10:32:53 AM
-class Solution {
-    public void reverseString(char[] s) {
-        int l = 0,r = s.length-1;
-        while(l<r){
-            char t = s[l];
-            s[l] = s[r];
-            s[r] = t;
-            l++;r--;
-        }
-    }
-}
+// Last updated: 20/7/2026, 2:01:49 pm
+1class Solution {
+2    public void reverse(char[] s, int l, int r) {
+3        if (l >= r) {
+4            return;
+5        }
+6        char t = s[l]; s[l] = s[r]; s[r] = t;
+7        l++; r--;
+8        reverse(s, l, r);
+9    }
+10
+11    public void reverseString(char[] s) {
+12        reverse(s, 0, s.length - 1);
+13    }
+14}

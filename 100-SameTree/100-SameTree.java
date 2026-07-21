224bed515132a6773e780e51f571c29aa45fbe42
Akarshana R <akarshanaraguu@gmail.com>
2026-07-21 23:08:35 +0530

    File updated at 21/7/2026, 11:08:33 pm

--- 100-SameTree/100-SameTree.java
+++ 100-SameTree/100-SameTree.java
@@ -1,23 +1,30 @@
-// Last updated: 6/4/2026, 10:34:42 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&&q==null) return true;
-        if(p==null||q==null) return false;
-        return (p.val==q.val) && isSameTree(p.left,q.left)&& isSameTree(p.right,q.right) ;
-    }
-}     
+// Last updated: 21/7/2026, 11:08:33 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18
+19        if (p == null && q == null)
+20            return true;
+21
+22        if (p == null || q == null)
+23            return false;
+24
+25        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+26        
+27    }
+28}
+29

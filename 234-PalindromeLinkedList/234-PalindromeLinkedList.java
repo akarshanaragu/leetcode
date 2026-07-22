2196c2f03c42c3ee1c5cc4c46a0e8d54a312f897
Akarshana R <akarshanaraguu@gmail.com>
2026-07-22 09:56:38 +0530

    File updated at 22/7/2026, 9:56:36 am

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -1,45 +1,34 @@
-// Last updated: 6/4/2026, 10:33:21 AM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        ListNode prev = null;
-        ListNode curr = slow;
-
-        while (curr != null) {
-            ListNode n = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = n;
-        }
-
-        ListNode a = head;
-        ListNode b = prev;
-
-        while (b != null) {
-            if (a.val != b.val) {
-                return false;
-            }
-            a = a.next;
-            b = b.next;
-        }
-        return true;
-    }
-}
+// Last updated: 22/7/2026, 9:56:36 am
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    
+13    ListNode curr;
+14
+15    public boolean isPalindrome(ListNode head) {
+16        curr = head;
+17        return solve(head);
+18    }
+19
+20    public boolean solve(ListNode head) {
+21        if (head == null) {
+22            return true;
+23        }
+24
+25        boolean b = solve(head.next) && curr.val == head.val;
+26
+27        curr = curr.next;
+28
+29        return b;
+30
+31    }
+32
+33}

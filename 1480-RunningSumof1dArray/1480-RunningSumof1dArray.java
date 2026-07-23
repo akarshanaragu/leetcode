e92c0ad2285345e9c1d9d5a76257d6758bc30fab
Akarshana R <akarshanaraguu@gmail.com>
2026-07-23 11:13:27 +0530

    File updated at 23/7/2026, 11:13:28 am

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,13 +1,17 @@
-// Last updated: 5/6/2026, 11:40:20 am
+// Last updated: 23/7/2026, 11:13:28 am
 1class Solution {
-2    public int[] getConcatenation(int[] nums) {
-3
-4        int n = nums.length;
-5        int arr[] = new int[2 * n];
-6        for (int i = 0; i < n; i++) {
-7            arr[i] = nums[i];
-8            arr[i + n] = nums[i];
-9        }
-10        return arr;
-11    }
-12}
+2    public void rec(int arr[],int i) {
+3        if (i == arr.length) {
+4            return;
+5        }
+6        arr[i] += arr[i - 1];
+7        rec(arr, i + 1);
+8    }
+9
+10    public int[] runningSum(int[] nums) {
+11        if (nums.length > 1) {
+12            rec(nums, 1);
+13        }
+14        return nums;
+15    }
+16}

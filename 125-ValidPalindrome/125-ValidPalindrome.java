e3be4421612c42b3b6c5d94d17cda418c9d64677
Akarshana R <akarshanaraguu@gmail.com>
2026-07-23 11:27:37 +0530

    File updated at 23/7/2026, 11:27:37 am

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,15 +1,18 @@
-// Last updated: 6/4/2026, 10:34:23 AM
-class Solution {
-    public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]","");
-        int l=0,r=s.length()-1;
-        while(l<r){
-            if(s.charAt(l)!=s.charAt(r)){
-                return false;
-            }
-            l++;r--;
-        }
-        return true;
-    }
-}
+// Last updated: 23/7/2026, 11:27:37 am
+1class Solution {
+2    public boolean rec(String s, int l, int r) {
+3        if (l >= r) {
+4            return true;
+5        }
+6        if (s.charAt(l) != s.charAt(r)) {
+7            return false;
+8        }
+9        return rec(s, l + 1, r - 1);
+10    }
+11
+12    public boolean isPalindrome(String s) {
+13        s = s.toLowerCase();
+14        s = s.replaceAll("[^a-z0-9]", "");
+15        return rec(s, 0, s.length() - 1);
+16    }
+17}

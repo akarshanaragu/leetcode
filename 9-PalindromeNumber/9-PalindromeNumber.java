72ad2fd2c318c65f667d514637d4b37982bcbc20
Akarshana R <akarshanaraguu@gmail.com>
2026-07-24 21:59:27 +0530

    File updated at 24/7/2026, 9:59:27 pm

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,14 +1,26 @@
-// Last updated: 6/4/2026, 10:35:51 AM
-class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-        int rev = 0;
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-
-        return x == rev || x == rev / 10;
-    }
-}
+// Last updated: 24/7/2026, 9:59:27 pm
+1class Solution {
+2
+3    int rev = 0;
+4
+5    public boolean isPalindrome(int x) {
+6        
+7        if (x < 0) 
+8            return false;
+9
+10        int original = x;
+11        reverse(x);
+12
+13        return original == rev;
+14    }
+15
+16    void reverse(int x) {
+17
+18        if (x == 0) 
+19            return;
+20
+21        rev = rev * 10 + x % 10;
+22        reverse(x / 10);
+23
+24    }
+25}

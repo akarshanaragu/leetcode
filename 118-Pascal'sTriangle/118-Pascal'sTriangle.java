7070cbcf06c13fb5930b8332a4ac88605f6d5874
Akarshana R <akarshanaraguu@gmail.com>
2026-08-05 21:58:35 +0530

    File updated at 5/8/2026, 9:58:35 pm

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -1,25 +1,23 @@
-// Last updated: 16/6/2026, 9:48:47 am
+// Last updated: 5/8/2026, 9:58:35 pm
 1class Solution {
-2    public List<List<Integer>> generate(int n) {
-3
-4        List<List<Integer>> res = new ArrayList<>();
-5
-6        for (int i = 0; i < n; i++) {
-7
-8            List<Integer> l = new ArrayList<>();
-9
-10            l.add(1);
+2    
+3    public List<List<Integer>> generate(int r) {
+4
+5        List<List<Integer>> res = new ArrayList<>();
+6
+7        for (int i = 0; i < r; i++) {
+8
+9            List<Integer> row = new ArrayList<>();
+10            int num = 1;
 11
-12            if (i > 0) {
-13                List<Integer> prev = res.get(i - 1);
-14                for (int j = 1; j < i; j++) {
-15                    l.add(prev.get(j-1)+prev.get(j));
-16                }
-17                l.add(1);
-18            }
-19            res.add(l);
-20        }
-21
-22        return res;
-23    }
-24}
+12            for (int j = 0; j <= i; j++) {
+13                row.add(num);
+14                num = num * (i - j) / (j + 1);
+15            }
+16
+17            res.add(row);
+18        }
+19
+20        return res;
+21    }
+22}

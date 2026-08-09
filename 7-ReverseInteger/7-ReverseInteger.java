e2f8b418f8d1ddbc8fb9ddc700e9741571bad4b3
Akarshana R <akarshanaraguu@gmail.com>
2026-08-09 21:27:54 +0530

    File updated at 9/8/2026, 9:27:54 pm

--- 7-ReverseInteger/7-ReverseInteger.java
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -1,23 +1,26 @@
-// Last updated: 6/4/2026, 10:35:52 AM
-class Solution {
-    public int reverse(int x) {
-        int rev = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-
-            if (rev > Integer.MAX_VALUE / 10 ||
-               (rev == Integer.MAX_VALUE / 10 && digit > 7))
-                return 0;
-
-            if (rev < Integer.MIN_VALUE / 10 ||
-               (rev == Integer.MIN_VALUE / 10 && digit < -8))
-                return 0;
-
-            rev = rev * 10 + digit;
-        }
-
-        return rev;
-    }
-}
+// Last updated: 9/8/2026, 9:27:54 pm
+1class Solution {
+2    
+3    public int reverse(int x) {
+4
+5        int r = 0;
+6
+7        while (x != 0) {
+8
+9            int d = x % 10; x /= 10;
+10
+11            if (r > Integer.MAX_VALUE / 10 || (r == Integer.MAX_VALUE / 10 && d > 7))
+12                return 0;
+13
+14            if (r < Integer.MIN_VALUE / 10 || (r == Integer.MIN_VALUE / 10 && d < -8))
+15                return 0;
+16
+17            r = r * 10 + d;
+18
+19        }
+20
+21        return r;
+22
+23    }
+24
+25}

c9aaaf074ae131c8ef2bf9681ed8323fa01f5e47
Akarshana R <akarshanaraguu@gmail.com>
2026-08-22 09:45:52 +0530

    File updated at 22/8/2026, 9:45:51 am

--- 4025-MinimizetheMaximumWaitingTimeatSynchronizedTrafficLights/4025-MinimizetheMaximumWaitingTimeatSynchronizedTrafficLights.java
+++ 4025-MinimizetheMaximumWaitingTimeatSynchronizedTrafficLights/4025-MinimizetheMaximumWaitingTimeatSynchronizedTrafficLights.java
@@ -1,18 +1,14 @@
-// Last updated: 22/8/2026, 9:42:38 am
+// Last updated: 22/8/2026, 9:45:51 am
 1class Solution {
 2    public int minPenalty(int p, int[] l, int[] a) {
-3        int max = 0,ans =0,w=0;
-4        for(int i=0;i<l.length;i++){
-5            max = Math.max(max,l[i]);
+3        int max = 0, ans = 0;
+4        for (int i = 0; i < l.length; i++) {
+5            max = Math.max(max, l[i]);
 6        }
-7        for(int i=0;i<a.length;i++){
-8            int x = a[i]%p;
-9            if(x>=max){
-10                w=p-x;
-11                System.out.println(p-max);
-12            }
-13            ans = Math.max(w,ans);
-14        }
-15        return ans;
-16    }
-17}
+7        for (int i = 0; i < a.length; i++) {
+8            if (a[i]%p >= max) 
+9                ans = Math.max(p-(a[i]%p), ans);
+10        }
+11        return ans;
+12    }
+13}

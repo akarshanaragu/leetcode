a4ed744568afd9609285a2d427c9246772f0a24a
Akarshana R <akarshanaraguu@gmail.com>
2026-09-24 15:09:36 +0530

    Create 1137-NThTribonacciNumber

---
+++ 1137-NThTribonacciNumber/1137-NThTribonacciNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 24/9/2026, 3:09:33 pm
+class Solution {
+
+    int[] dp = new int[38];
+
+    public int tribonacci(int n) {
+
+        if (n == 0)
+            return 0;
+            
+        if (n == 1 || n == 2)
+            return 1;
+
+        if (dp[n] != 0)
+            return dp[n];
+
+        dp[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        return dp[n];
+    }
+}

79fc010f817e3abae70a847da3224a2aa0494fb3
Akarshana R <akarshanaraguu@gmail.com>
2026-09-24 15:07:39 +0530

    Create 1863-SumOfAllSubsetXorTotals

---
+++ 1863-SumOfAllSubsetXorTotals/1863-SumOfAllSubsetXorTotals.cpp
@@ -0,0 +1,16 @@
+// Last updated: 24/9/2026, 3:07:37 pm
+class Solution {
+public:
+int n;
+    int f(int i, int xorsum, vector<int>& nums){
+        if (i==n) return xorsum;
+        int x=nums[i];
+        int take=f(i+1, xorsum^x, nums);
+        int notake=f(i+1, xorsum, nums);
+        return take+notake;
+    }
+    int subsetXORSum(vector<int>& nums) {
+        n=nums.size();
+        return f(0, 0, nums);
+    }
+};
